3bca6dbf37264cad20f2a0d40d3e4366a9136c99
dwqs <461147874@qq.com>
2016-06-06 01:07:13 +0800

    article read for tc

--- client/contents/tuicool/index.js
+++ client/contents/tuicool/index.js
@@ -101,13 +101,13 @@ export default class TuiCool extends Component {
     }
 
     listener (originUrl){
-        // return (e) => {
-        //     e.stopPropagation();
-        //     if (e.target.nodeName.toLowerCase() === 'a' || e.target.nodeName.toLowerCase() === 'h3'){
-        //         return;
-        //     }
-        //     this.props.open(originUrl);
-        // };
+        return (e) => {
+            e.stopPropagation();
+            if (e.target.nodeName.toLowerCase() === 'a' || e.target.nodeName.toLowerCase() === 'h3'){
+                return;
+            }
+            this.props.open(originUrl);
+        };
     }
 
     renderPostList () {
