0913e9326df42d0a897a5105d6430bbc121d9161
dwqs <461147874@qq.com>
2016-11-15 10:44:50 +0800

    fix

--- server/router/toutiao/toutiaoLib.js
+++ server/router/toutiao/toutiaoLib.js
@@ -11,15 +11,15 @@ exports.parseList = function (lists) {
         let titleObj = $(list).find('.title');
         let title = titleObj.text();
         let originUrl = titleObj.children('a').attr('href');
-        let meta = $(list).find('.meta')[0].firstChild.nodeValue;;
-        let avatarUrl = $(list).find('img').attr('src');;
+        let meta = $(list).find('.meta')[0].firstChild.nodeValue;
+        let avatarUrl = $(list).find('img').attr('src');
         let subjectUrl = $(list).find('.subject-name a').attr('href');
         let subjectOriginUrl = `http://toutiao.io${subjectUrl}`;
         let subjectText = $(list).find('.subject-name a').text();
 
         return {
             listTitle:title,
-            listOriginUrl: originUrl,
+            listOriginUrl: `https://toutiao.io${originUrl}`,
             listMeta: meta,
             listAvatarUrl: avatarUrl,
             listSubjectUrl: subjectOriginUrl,
